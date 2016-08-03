5980dfdeb640cbfe47826cd1fdfe336778167f81
Kyungtae Lee <ktlee@bitnine.net>
2016-08-03 23:26:51 +0900

    fix: Add a copy function for a 'CypherCreate' plan node
    * server-side cached plan features is used through
      stmt.execute("CREATE ()")
    
    Closes #39

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -4201,6 +4201,21 @@ _copyCypherName(const CypherName *from)
 	return newnode;
 }
 
+static CypherCreate *
+_copyCypherCreate(const CypherCreate *from)
+{
+	CypherCreate *newnode = makeNode(CypherCreate);
+
+	CopyPlanFields((const Plan *) from, (Plan *) newnode);
+
+	COPY_SCALAR_FIELD(operation);
+	COPY_SCALAR_FIELD(canSetTag);
+	COPY_NODE_FIELD(subplan);
+	COPY_NODE_FIELD(graphPattern);
+
+	return newnode;
+}
+
 /* ****************************************************************
  *					pg_list.h copy functions
  * ****************************************************************
@@ -5083,6 +5098,9 @@ copyObject(const void *from)
 			break;
 		case T_CypherName:
 			retval = _copyCypherName(from);
+			break;
+		case T_CypherCreate:
+			retval = _copyCypherCreate(from);
 			break;
 
 		default:
