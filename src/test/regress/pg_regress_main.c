36b159c2d7338aa4199000646aafaac3e26230f3
Junseok Yang <jsyang@bitnine.net>
2017-01-15 19:27:21 -0800

    chore: Use `agens` instead of `psql` in the regression tests

--- src/test/regress/pg_regress_main.c
+++ src/test/regress/pg_regress_main.c
@@ -64,7 +64,7 @@ psql_start_test(const char *testname,
 						   "%s ", launcher);
 
 	snprintf(psql_cmd + offset, sizeof(psql_cmd) - offset,
-			 "\"%s%spsql\" -X -a -q -d \"%s\" < \"%s\" > \"%s\" 2>&1",
+			 "\"%s%sagens\" -X -a -q -d \"%s\" < \"%s\" > \"%s\" 2>&1",
 			 bindir ? bindir : "",
 			 bindir ? "/" : "",
 			 dblist->str,
