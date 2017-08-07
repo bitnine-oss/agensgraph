04a693c593e85a7874ceacb156b3d42a903083d1
Junseok Yang <jsyang@bitnine.net>
2017-08-07 14:10:29 +0900

    fix: Add `const` qualifier to remove compilation warnings

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -15385,7 +15385,7 @@ dumpTableSchema(Archive *fout, TableInfo *tbinfo)
 			default:
 				{
 					/* check if it's table or label  */
-					char	   *id;
+					const char *id;
 					PQExpBuffer query = createPQExpBuffer();
 					PGresult   *res;
 					char		labkind[2] = {'\0'};
