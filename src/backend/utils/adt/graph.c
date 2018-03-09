9f83ab72db85caad928b8b490d006bb60c02c10c
Hyeongtae Lim <htlim@bitnine.net>
2018-03-09 10:24:30 +0900

    fix: Leave CID unchanged when calling `startnode()` and `endnode()`

--- src/backend/utils/adt/graph.c
+++ src/backend/utils/adt/graph.c
@@ -1119,7 +1119,7 @@ getEdgeVertex(HeapTupleHeader edge, EdgeVertexKind evk)
 	if (SPI_connect() != SPI_OK_CONNECT)
 		elog(ERROR, "SPI_connect failed");
 
-	ret = SPI_execute_with_args(sqlcmd, 2, argTypes, values, NULL, false, 0);
+	ret = SPI_execute_with_args(sqlcmd, 2, argTypes, values, NULL, true, 0);
 	if (ret != SPI_OK_SELECT)
 		elog(ERROR, "SPI_execute failed: %s", sqlcmd);
 
