d428055ab05fe91c2581dfd2195905b940eade93
Hyeongtae Lim <htlim@bitnine.net>
2018-03-19 09:46:54 +0900

    fix: Error occurs when use delete clause after optional match
    - If the user tries to remove a null object, notice instead of an error.
    
    Closes #330

--- src/backend/executor/nodeModifyGraph.c
+++ src/backend/executor/nodeModifyGraph.c
@@ -944,9 +944,13 @@ ExecDeleteGraph(ModifyGraphState *mgstate, TupleTableSlot *slot)
 		econtext->ecxt_scantuple = slot;
 		datum = ExecEvalExpr(e, econtext, &isNull);
 		if (isNull)
-			ereport(ERROR,
+		{
+			ereport(NOTICE,
 					(errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED),
-					 errmsg("deleting NULL is not allowed")));
+					 errmsg("skipping deletion of NULL graph element")));
+
+			continue;
+		}
 
 		if (mgstate->eagerness)
 		{
