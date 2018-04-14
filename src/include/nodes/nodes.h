18862d95dc2df12647628cad2145e4790ec625ff
Junseok Yang <jsyang@bitnine.net>
2018-04-13 17:01:44 -0700

    feat: Remove `JOIN_CYPHER_DETACH`
    (cherry picked from commit baca1e7b31492f8f7e4d8a0a4e74c7338598d7f2)

--- src/include/nodes/nodes.h
+++ src/include/nodes/nodes.h
@@ -765,7 +765,6 @@ typedef enum JoinType
 
 	/* For Cypher DELETE */
 	JOIN_CYPHER_DELETE,
-	JOIN_CYPHER_DETACH
 
 	/*
 	 * We might need additional join types someday.
@@ -793,7 +792,6 @@ typedef enum JoinType
 	   (1 << JOIN_RIGHT) | \
 	   (1 << JOIN_CYPHER_MERGE) | \
 	   (1 << JOIN_CYPHER_DELETE) | \
-	   (1 << JOIN_CYPHER_DETACH) | \
 	   (1 << JOIN_ANTI))) != 0)
 
 /*
