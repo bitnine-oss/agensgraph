7cd8a39cc11b8af0d8b1a1e43f0a493523f69e5b
Hyeongtae Lim <htlim@bitnine.net>
2018-05-24 15:20:14 +0900

    fix: VLE returns incorrect result with sequential scan
    Closes #369

--- src/backend/executor/nodeNestloopVle.c
+++ src/backend/executor/nodeNestloopVle.c
@@ -63,6 +63,8 @@ static void addOuterRowidAndGid(NestLoopVLEState *node, TupleTableSlot *slot);
 static void addInnerRowidAndGid(NestLoopVLEState *node, TupleTableSlot *slot);
 static void addRowidAndGid(NestLoopVLEState *node, Datum rowid, Datum gid);
 static void popRowidAndGid(NestLoopVLEState *node);
+static TupleTableSlot *copyTupleTableSlot(TupleTableSlot *dstslot,
+										  TupleTableSlot *srcslot);
 
 
 TupleTableSlot *
@@ -185,9 +187,13 @@ ExecNestLoopVLE(NestLoopVLEState *node)
 			}
 			else
 			{
+				TupleTableSlot *prev_slot;
+
 				ENLV1_printf("no inner tuple, upscanning inner plan, looping");
 				ExecUpScan(innerPlan);
-				econtext->ecxt_outertuple = restoreStartAndBindVar(node);
+
+				prev_slot = restoreStartAndBindVar(node);
+				copyTupleTableSlot(econtext->ecxt_outertuple, prev_slot);
 				bindNestParam(nlv, econtext, econtext->ecxt_outertuple, NULL);
 			}
 
@@ -774,4 +780,20 @@ popRowidAndGid(NestLoopVLEState *node)
 	popElem(&node->rowids);
 	if (node->hasPath)
 		popElem(&node->path);
+}
+
+static TupleTableSlot *
+copyTupleTableSlot(TupleTableSlot *dstslot, TupleTableSlot *srcslot)
+{
+	int natts = srcslot->tts_tupleDescriptor->natts;
+
+	ExecSetSlotDescriptor(dstslot, srcslot->tts_tupleDescriptor);
+
+	/* shallow copy */
+	memcpy(dstslot->tts_values, srcslot->tts_values, natts * sizeof(Datum));
+	memcpy(dstslot->tts_isnull, srcslot->tts_isnull, natts * sizeof(bool));
+
+	ExecStoreVirtualTuple(dstslot);
+
+	return dstslot;
 }
