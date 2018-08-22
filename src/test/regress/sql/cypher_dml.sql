747ecd225fe49feb5f7dde4e74f619efdda4560c
Hyeongtae Lim <htlim@bitnine.net>
2018-08-22 18:06:30 +0900

    fix: Invalid dijkstra resultset

--- src/test/regress/sql/cypher_dml.sql
+++ src/test/regress/sql/cypher_dml.sql
@@ -296,49 +296,49 @@ CREATE (:time {sec: 1})-[:goes]->
 CREATE (:time {sec: 9})-[:goes*1..2]->(:time {sec: 10});
 
 MATCH (a:time)-[x:goes*3]->(b:time)
-RETURN a.sec AS a, length(x) AS x, b.sec AS b;
+RETURN a.sec AS a, length(x) AS x, b.sec AS b ORDER BY a;
 
 MATCH (a:time)-[x:goes*0]->(b:time)
-RETURN a.sec AS a, x, b.sec AS b;
+RETURN a.sec AS a, x, b.sec AS b ORDER BY a;
 
 MATCH (a:time)-[x:goes*0..1]->(b:time)
-RETURN a.sec AS a, length(x) AS x, b.sec AS b;
+RETURN a.sec AS a, length(x) AS x, b.sec AS b ORDER BY a;
 
 MATCH (a:time)-[x:goes*..1]->(b:time)
-RETURN a.sec AS a, length(x) AS x, b.sec AS b;
+RETURN a.sec AS a, length(x) AS x, b.sec AS b ORDER BY a;
 
 MATCH (a:time)-[x:goes*0..]->(b:time)
-RETURN a.sec AS a, length(x) AS x, b.sec AS b;
+RETURN a.sec AS a, length(x) AS x, b.sec AS b ORDER BY a;
 
 MATCH (a:time)-[x:goes*3..6]->(b:time)
-RETURN a.sec AS a, length(x) AS x, b.sec AS b;
+RETURN a.sec AS a, length(x) AS x, b.sec AS b ORDER BY a;
 
 MATCH (a:time)-[x:goes*2]->(b:time)-[y:goes]->(c:time)-[z:goes*2]->(d:time)
 RETURN a.sec AS a, length(x) AS x,
        b.sec AS b, type(y) AS y,
-       c.sec AS c, length(z) AS z, d.sec AS d;
+       c.sec AS c, length(z) AS z, d.sec AS d ORDER BY a;
 
 MATCH (a:time)-[x:goes*2]->(b:time)
 MATCH (b)-[y:goes]->(c:time)
 MATCH (c)-[z:goes*2]->(d:time)
 RETURN a.sec AS a, length(x) AS x,
        b.sec AS b, type(y) AS y,
-       c.sec AS c, length(z) AS z, d.sec AS d;
+       c.sec AS c, length(z) AS z, d.sec AS d ORDER BY a;
 
 MATCH (d:time)<-[z:goes*2]-(c:time)<-[y:goes]-(b:time)<-[x:goes*2]-(a:time)
 RETURN d.sec AS d, length(z) AS z,
        c.sec AS c, type(y) AS y,
-       b.sec AS b, length(x) AS x, a.sec AS a;
+       b.sec AS b, length(x) AS x, a.sec AS a ORDER BY d;
 
 MATCH (d:time)<-[z:goes*2]-(c:time)
 MATCH (c)<-[y:goes]-(b:time)
 MATCH (b)<-[x:goes*2]-(a:time)
 RETURN d.sec AS d, length(z) AS z,
        c.sec AS c, type(y) AS y,
-       b.sec AS b, length(x) AS x, a.sec AS a;
+       b.sec AS b, length(x) AS x, a.sec AS a ORDER BY d;
 
 MATCH (a:time)-[x*0..2]-(b)
-RETURN a.sec AS a, length(x) AS x, b.sec AS b;
+RETURN a.sec AS a, length(x) AS x, b.sec AS b ORDER BY a;
 
 -- VLE with graph path
 MATCH p = (:time)-[:goes*0]->(:time)
@@ -525,187 +525,6 @@ MATCH ({id:1})-[r:rel*]->() RETURN length(r) AS len ORDER BY len;
 
 SET enable_indexscan = default;
 SET enable_seqscan = default;
-
--- shortestpath(), allshortestpaths()
-SET graph_path = t;
-
-CREATE OR REPLACE FUNCTION ids(vertex[]) RETURNS int[] AS $$
-DECLARE
-  v vertex;
-  vids int[];
-BEGIN
-  IF $1 IS NULL THEN
-    RETURN ARRAY[]::int[];
-  END IF;
-  FOREACH v IN ARRAY $1 LOOP
-    vids = array_append(vids, (v->>'id')::int);
-  END LOOP;
-  RETURN vids;
-END;
-$$ LANGUAGE plpgsql;
-
--- 1->2->3->4->5
-MATCH (a:person {id: 1})-[k:knows]->(b:person {id: 5}) DELETE k;
-MATCH (a:person {id: 5})-[k:knows]->(b:person {id: 6}) DELETE k;
-MATCH (a:person {id: 6}) DELETE a;
-MATCH (a:person {id: 4}), (b:person {id: 5}) CREATE (a)-[:knows]->(b);
-
-MATCH (p:person), (f:person) WHERE p.id = 3 AND f.id = 4
-RETURN ids(nodes(shortestpath((p)-[:knows]->(f)))) AS ids;
-
-MATCH (p:person), (f:person) WHERE p.id = 3 AND f.id = 5
-RETURN ids(nodes(shortestpath((p)-[:knows]->(f)))) AS ids;
-
-MATCH (p:person), (f:person) WHERE p.id = 3
-RETURN ids(nodes(shortestpath((p)<-[:knows]-(f)))) AS ids;
-
-MATCH (p:person), (f:person) WHERE p.id = 3
-RETURN ids(nodes(shortestpath((p)-[:knows*]-(f)))) AS ids;
-
-MATCH (p:person), (f:person), x=shortestpath((p)-[:knows*]-(f))
-WHERE p.id = 3
-RETURN ids(nodes(x)) AS ids;
-
-MATCH x=shortestpath((p:person)-[:knows*]-(f:person))
-WHERE p.id = 3
-RETURN ids(nodes(x)) AS ids;
-
-MATCH (p:person), (f:person) WHERE p.id = 3
-RETURN ids(nodes(shortestpath((p)-[:knows*0..1]-(f)))) AS ids;
-
-MATCH (p:person), (f:person) WHERE p.id = 1
-RETURN ids(nodes(shortestpath((p)-[:knows*2..]->(f)))) AS ids;
-
-MATCH (p:person), (f:person) WHERE p.id = 3 AND f.id = 5
-CREATE (p)-[:knows]->(:person {id: 6})-[:knows]->(f);
-
-MATCH (p:person), (f:person) WHERE p.id = 1 AND f.id = 5
-RETURN length(allshortestpaths((p)-[:knows*]-(f))) AS cnt;
-
-CREATE VLABEL v;
-CREATE ELABEL e;
-
-CREATE (:v {id: 0});
-CREATE (:v {id: 1});
-CREATE (:v {id: 2});
-CREATE (:v {id: 3});
-CREATE (:v {id: 4});
-CREATE (:v {id: 5});
-CREATE (:v {id: 6});
-
-MATCH (v1:v {id: 0}), (v2:v {id: 4})
-CREATE (v1)-[:e {weight: 3}]->(v2);
-MATCH (v1:v {id: 0}), (v2:v {id: 1})
-CREATE (v1)-[:e {weight: 7}]->(v2);
-MATCH (v1:v {id: 0}), (v2:v {id: 5})
-CREATE (v1)-[:e {weight: 10}]->(v2);
-
-MATCH (v1:v {id: 4}), (v2:v {id: 6})
-CREATE (v1)-[:e {weight: 5}]->(v2);
-MATCH (v1:v {id: 4}), (v2:v {id: 3})
-CREATE (v1)-[:e {weight: 11}]->(v2);
-MATCH (v1:v {id: 4}), (v2:v {id: 1})
-CREATE (v1)-[:e {weight: 2}]->(v2);
-
-MATCH (v1:v {id: 1}), (v2:v {id: 3})
-CREATE (v1)-[:e {weight: 10}]->(v2);
-MATCH (v1:v {id: 1}), (v2:v {id: 2})
-CREATE (v1)-[:e {weight: 4}]->(v2);
-MATCH (v1:v {id: 1}), (v2:v {id: 5})
-CREATE (v1)-[:e {weight: 6}]->(v2);
-
-MATCH (v1:v {id: 5}), (v2:v {id: 3})
-CREATE (v1)-[:e {weight: 9}]->(v2);
-
-MATCH (v1:v {id: 6}), (v2:v {id: 3})
-CREATE (v1)-[:e {weight: 4}]->(v2);
-
-MATCH (v1:v {id: 2}), (v2:v {id: 3})
-CREATE (v1)-[:e {weight: 2}]->(v2);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1)-[e:e]->(v2), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v2)<-[e:e]-(v1), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1)-[e:e]-(v2), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v2)-[e:e]-(v1), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1)-[e:e]->(v2), e.weight + 1)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1)-[e:e]->(v2), e.weight, e.weight >= 5)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1)-[e:e]->(v2), e.weight, e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1:v {id: 0})-[e:e]->(v2), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      path=dijkstra((v1:v)-[e:e*1..]->(v2), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v {id: 6}), (v2:v {id: 2}),
-      path=dijkstra((v1:v)-[e:e]->(v2), e.weight)
-RETURN nodes(path);
-
-MATCH (v1:v), (v2:v),
-      path=dijkstra((v1:v)-[e:e]->(v2), e.weight)
-WHERE v1.id = 6 AND v2.id = 2
-RETURN nodes(path);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3})
-RETURN dijkstra((v1)-[e:e]->(v2), e.weight);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 0})
-RETURN dijkstra((v1)-[e:e]->(v2), e.weight);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      p=dijkstra((v1)-[:e]->(v2), 1)
-RETURN nodes(p);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-      p=dijkstra((v1)-[:e]->(v2), 1, LIMIT 10)
-RETURN nodes(p);
-
-MATCH (:v {id: 4})-[e:e]-(:v {id: 6})
-SET e.weight = 4;
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-	  (path, x)=dijkstra((v1)-[e:e]->(v2), e.weight, LIMIT 2)
-RETURN nodes(path), x;
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-	  (path, x)=dijkstra((v2)<-[e:e]->(v1), e.weight, LIMIT 2)
-RETURN nodes(path), x;
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3})
-RETURN dijkstra((v1)-[e:e]->(v2), e.weight, LIMIT 2);
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-	  (path, x)=dijkstra((v1)-[e:e]->(v2), e.weight, LIMIT 0)
-RETURN nodes(path), x;
-
-MATCH (:v {id: 4})-[e:e]-(:v {id: 6})
-SET e.weight = -1;
-
-MATCH (v1:v {id: 0}), (v2:v {id: 3}),
-	  (path, x)=dijkstra((v1)-[e:e]->(v2), e.weight, LIMIT 10)
-return nodes(path), x;
 
 SET graph_path = agens;
 
