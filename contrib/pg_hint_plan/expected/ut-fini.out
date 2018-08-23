7f62b7cfe7eb4e2339fd1851423d657b568b1231
Hyeongtae Lim <htlim@bitnine.net>
2018-08-23 13:44:19 +0900

    fix: Error occurs in contrib - Remove unnecessary extensions(postgresql_hll, hadoop_fdw, pg_statsinfo, pg_hint_plan)

--- contrib/pg_hint_plan/expected/ut-fini.out
+++
@@ -1,2 +0,0 @@
-DROP ROLE IF EXISTS super_user;
-DROP ROLE IF EXISTS normal_user;
--- contrib/pg_statsinfo/agent/bin/uninstall_pg_statsrepo.sql
+++
@@ -1,7 +0,0 @@
-/*
- * bin/uninstall_pg_statsrepo.sql
- *
- * Copyright (c) 2009-2018, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
- */
-
-DROP SCHEMA IF EXISTS statsrepo CASCADE;
