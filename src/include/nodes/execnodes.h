63cf267ab56e80d7303be5cd4571b39a92ec8fa8
Hyunsu Choi <hyunsu.choi@bitnet.net>
2018-09-27 17:51:32 +0900

    fix: fix incorrect spacePeak of shortestpath

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -2205,6 +2205,7 @@ typedef struct Hash2SideState
 	PlanState      *spstate;
 	double          totalPaths;
 	long            hops;
+	Size            spacePeak;
 } Hash2SideState;
 
 typedef struct ShortestpathState
