a41847a726236584fff3183e3adf75e18a03ff7a
Alex Kwak <take-me-home@kakao.com>
2022-06-03 11:59:03 +0900

    fix: Common function definitions for CypherAccessor.

---
+++ src/include/parser/parse_cypher_utils.h
@@ -0,0 +1,19 @@
+/*
+ * parse_cypher_utils.h
+ *
+ * Copyright (c) 2022 by Bitnine Global, Inc.
+ *
+ * IDENTIFICATION
+ *	  src/include/parser/parse_cypher_utils.h
+ */
+
+#ifndef AGENSGRAPH_PARSE_CYPHER_UTILS_H
+#define AGENSGRAPH_PARSE_CYPHER_UTILS_H
+
+#include "parser/parse_node.h"
+
+Node *makeJsonbFuncAccessor(ParseState *pstate, Node *expr, List *path);
+bool IsJsonbAccessor(Node *expr);
+void getAccessorArguments(Node *node, Node **expr, List **path);
+
+#endif
