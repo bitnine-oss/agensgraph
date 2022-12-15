20e19891b47096d32b65f1c06b8cd4cf9f35a0d2
Alex Kwak <take-me-home@kakao.com>
2022-12-15 13:51:29 +0900

    fix: Includes necessary JOINs of vertices (#599)
    Previously, a patch(3ced8823b026951549ef05f88cf4c479cb6080fb) was created to
    remove the items that A thought were unnecessary joins. However, this returned
    an exceptional case for the self-joining case without path(other words, Unnamed
    path).
    
    so, we rollback that partial of patch. and, we will refactor pattern match
    logic for performance issue.

--- src/test/regress/sql/cypher_dml2.sql
+++ src/test/regress/sql/cypher_dml2.sql
@@ -42,3 +42,25 @@ MATCH(n)-[e*3]->(n3) RETURN last(e);
 
 
 DROP GRAPH cypher_dml2 CASCADE;
+
+CREATE GRAPH cypher_dml2;
+SET GRAPH_PATH to cypher_dml2;
+
+CREATE ({id: 1})-[:e1]->({id: 2})-[:e1]->({id: 3})-[:e1]->({id: 4})
+RETURN *;
+
+MATCH (a {id: 1}), (b {id: 1})
+CREATE (b)-[:e1]->(a)
+RETURN *;
+
+MATCH (a)
+RETURN *;
+
+MATCH (a)-[]-(a) RETURN *;
+MATCH p=(a)-[]-(a) RETURN *;
+
+EXPLAIN VERBOSE MATCH (a)-[]-(a) RETURN a;
+EXPLAIN VERBOSE MATCH (a)-[]-(a) RETURN *;
+EXPLAIN VERBOSE MATCH p=(a)-[]-(a) RETURN *;
+
+DROP GRAPH cypher_dml2 CASCADE;
