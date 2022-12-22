c9c8a8d2dd35be2b996e28a653c55661e23fed63
Alex Kwak <take-me-home@kakao.com>
2022-12-22 14:02:36 +0900

    fix: Supports VERTICES, EDGES on cypher list predicate functions. (#603)
    Previously, predicate function must requires JSONB(Array) type. so, Array
    type(e.g., Vertices or Edges) cannot be used.
    
    so, Makes CypherListComp supports Vertex and Edge Array, and uses their
    properties.
    
    additionally, implement `isEmpty(Jsonb)` function.

--- src/backend/utils/adt/cypher_empty_funcs.c
+++ src/backend/utils/adt/cypher_empty_funcs.c
@@ -11,9 +11,36 @@
 
 #include "utils/cypher_empty_funcs.h"
 #include "utils/fmgrprotos.h"
+#include "utils/jsonb.h"
 
 Datum
 cypher_to_jsonb(PG_FUNCTION_ARGS)
 {
 	return to_jsonb(fcinfo);
+}
+
+Datum
+cypher_isempty_jsonb(PG_FUNCTION_ARGS)
+{
+	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
+
+	if (JB_ROOT_IS_SCALAR(jb))
+	{
+		JsonbValue *sjv;
+
+		sjv = getIthJsonbValueFromContainer(&jb->root, 0);
+		if (sjv->type == jbvString)
+		{
+			PG_RETURN_BOOL(sjv->val.string.len <= 0);
+		}
+	}
+	else if (JB_ROOT_IS_ARRAY(jb) || JB_ROOT_IS_OBJECT(jb))
+	{
+		PG_RETURN_BOOL(JB_ROOT_COUNT(jb) <= 0);
+	}
+
+	ereport(ERROR,
+			(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			 errmsg("isEmpty(): list or object or string is expected but %s",
+					JsonbToCString(NULL, &jb->root, VARSIZE(jb)))));
 }
